1c28d9814e741f8e982792b4042f2cdaaa38a64b
Andrew White <white.d.andrew@gmail.com>
2021-11-02 21:44:01 -0400

    Completed first draft

--- rxn-speed/Assets/TouchInput.cs
+++ rxn-speed/Assets/TouchInput.cs
@@ -1,57 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchInput : MonoBehaviour
 {
 
     private float waitTime = -3;
     private float elapsed = 0;
+    private float totalElapsed = 0;
     bool waiting = false;
     bool measuring = false;
+    private int attempt = 0;
+
+    public Text output;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    private bool touchStart()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    private bool touchStop()
+    {
+        return Input.GetMouseButtonUp(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
         elapsed += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (touchStart())
         {
-            waiting = true;
-            elapsed = 0;
-            waitTime = Random.Range(1, 2);
-            Debug.Log(waitTime);
+            BeginWait();
         }
 
-        if (waitTime > 0 && elapsed >= waitTime)
+        if (waiting)
         {
-            ChangeColor(Color.red);
-            measuring = true;
-            waiting = false;
-            elapsed = 0;
-            waitTime = -1;
+            if (elapsed >= waitTime)
+            {
+                BeginMeasure();
+            }
+            else if (touchStop())
+            {
+                log("Failed");
+                ResetMeasure();
+            }
+
         }
 
-        if(measuring && Input.GetKeyUp(KeyCode.Space))
+        if (measuring)
         {
-            Debug.Log("Reaction Speed = " + elapsed);
-            measuring = false;
-            elapsed = 0;
-            ChangeColor(Color.white);
+            if (touchStop())
+            {
+                log((elapsed * 100).ToString("F3") + "ms");
+                ResetMeasure();
+            }
+            else if (elapsed > 2)
+            {
+                log("Failed");
+                ResetMeasure();
+            }
         }
     }
+
+    void log(string m)
+    {
+        var minutes = Time.realtimeSinceStartup / 60;
+        output.text += attempt.ToString("F3") + "@" + minutes.ToString("F3") + "min | " + m + "\n";
+    }
+
+    void BeginWait()
+    {
+        waiting = true;
+        elapsed = 0;
+        waitTime = Random.Range(0.75f, 2);
+        ChangeColor(Color.green);
+    }
+
+    void BeginMeasure()
+    {
+        ChangeColor(Color.red);
+        measuring = true;
+        waiting = false;
+        elapsed = 0;
+        waitTime = -1;
+        attempt++;
+    }
+
+    void ResetMeasure()
+    {
+        measuring = false;
+        waiting = false;
+        elapsed = 0;
+        ChangeColor(Color.white);
+    }
+
 
     void ChangeColor(Color c)
     {
         var cubeRenderer = gameObject.GetComponent<SpriteRenderer>();
-       
+
         cubeRenderer.color = c;
     }
 }
